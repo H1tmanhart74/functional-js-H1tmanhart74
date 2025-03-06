cdffadc36902fac265b4074e6f8c51129732e2d6
John Hart <john.hart003@student.swic.edu>
2025-03-06 02:26:47 -0600

    Examples of different function styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-//Known things we can store in variables
-const x = 3;
-const y = "Hello";
-const z = {name : "John"};
-
-console.log(x);
-console.log(y);
-console.log(z);
-
-//functions can also be stored in variables
+//Two different styles to functions
+//This is the normal style of functions 
 const sayHello = function(){
     return "Hello";
 }
 console.log(sayHello());
+
+//This is the arrow function style
+const sayGoodbye = () => {
+    return "Goodbye";
+}
+console.log(sayGoodbye());
+
+//another way to write the arrow function
+const sayHelloAgain = () => "Hello Again";
+console.log(sayHelloAgain());
