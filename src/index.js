fd5dc82ebf2311b2b9f1df63c00c612e2c89bc1e
John Hart <john.hart003@student.swic.edu>
2025-03-06 10:28:43 -0600

    Example JavaScript with some HTML

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-/* 
-More on the map method.
-It iterates over each element in the array so you don't have to specify the index or length of the array.
-It creates a new array with the results by calling a provided function on every element in the calling array.
-It does not change the original array. It returns a new array.
-It must return something from the callback function. The array will be undefined if the function doesn't return anything.
-*/
-//Doom video example
+//JavaScript with some HTML
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map(((number) => `<li>${number * 2}</li>`
+));
+
+console.log(doubledNumbers);
+
+//Video example of JavaScript with some HTML
+
 const backpackFullOfAmmo = [
-    { item: 'bullets', quantity: 10, priceEa: 0.5},
-    { item: 'shotgun shells', quantity: 4, priceEa: 1.0},
-    { item: 'rockets', quantity: 1, priceEa: 5.0},
-    { item: 'energy cell', quantity: 20, priceEa: 0.2},
+    {name: 'bullets', quantity: 10, price: 0.5},
+    {name: 'shotgun shells', quantity: 4, price: 0.5},
+    {name: 'rockets', quantity: 1, price: 5.0},
+    {name: 'energy cell units', quantity: 5, price: 0.2},
 ]
 
-function calculateOrderCost(order) {
-    return order.quantity * order.priceEa
-}
-
-const totalValue = backpackFullOfAmmo.map((ammo) => {
-    console.log(ammo)
-
-    return calculateOrderCost(ammo)
-    /* 
-    if this isn't here it will return undefined in the array
-    we can control what we want to return within the map method
-    we can create an array saying hi or in this case we are returning the quantity times the price.
-    */
-    
+const tableRows = backpackFullOfAmmo.map((ammo) => {
+    return `
+    <tr>
+        <td>${ammo.name}</td>
+        <td>${ammo.quantity}</td>
+        <td>${ammo.price}</td>
+    </tr>
+    `
 })
-
-console.log(totalValue)
+.join('\n'); //this is a join that will separate each array with a new line (\n)
+console.log(tableRows);
