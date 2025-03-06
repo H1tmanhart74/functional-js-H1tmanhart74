2c763e4b3742a2364a0fd3896fc838eaf14bc1a0
John Hart <john.hart003@student.swic.edu>
2025-03-05 23:46:56 -0600

    Examples of what variables can store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-var x = 3;
+//Known things we can store in variables
+const x = 3;
+const y = "Hello";
+const z = {name : "John"};
 
 console.log(x);
+console.log(y);
+console.log(z);
+
+//functions can also be stored in variables
+const sayHello = function(){
+    return "Hello";
+}
+console.log(sayHello());
