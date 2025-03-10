510b43674f364df5e077a83d115229fa22d71b7b
John Hart <john.hart003@student.swic.edu>
2025-03-10 12:52:21 -0500

    Add solution to coding challenge one

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -1,12 +1,16 @@
-//This is an example for the sort method
-//Our classic doom players example
-const players = [
-    {name: 'DoomGuy', frags: 25, deaths: 0},
-    {name: 'Slayer', frags: 25, deaths: 1},
-    {name: 'Marine', frags: 12, deaths: 15},
-    {name: 'Noob', frags: 0, deaths: 25},
+//Coding Challenge #1
+//Using this data we need to figure out how to calculate the total value of the backpack.
+const backpackFullOfAmmo = [
+    { item: 'bullets', quantity: 10, priceEa: 0.5 }, // 5
+    { item: 'shotgun shells', quantity: 4, priceEa: 0.5 }, // 2
+    { item: 'rockets', quantity: 1, priceEa: 5.0 }, // 5
+    { item: 'energy cell units', quantity: 20, priceEa: 0.2 }, // 4
   ];
-//The sort method is used to sort the elements of an array. In this case it will sort the players by the number of frags they have.
-const sortedPlayers = players.sort((a, b) => b.frags - a.frags); //It also uses a minus instead of a plus to sort in descending order
+  //In order to find the total we need to go through each item in the array and multiply the quantity by the price.
+  //We can use the reduce method to do this.
+    const totalValue = backpackFullOfAmmo.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.quantity * currentValue.priceEa;
+    }, 0);//The accumulator starts at 0, then the current value is multiplied by the quantity and price to get 5,2,5,4. Then the accumulator adds each value to get the total value.
+    //The math works like this. 0 + 5 = 5, 5 + 2 = 7, 7 + 5 = 12, 12 + 4 = 16.
 
-console.log(sortedPlayers); //This will show the sort of every frag in descending order
+    console.log(totalValue); //Total Value is 16
