633cfc93a0784d244a7f9f067f3292ea0c1c6cc7
John Hart <john.hart003@student.swic.edu>
2025-03-09 22:44:29 -0500

    Add reduce method example code

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -1,19 +1,21 @@
-//Find the players with more frags than deaths and display them
+//This is an example for the reduce method
+//Our classic doom players example
 const players = [
-  {name: 'DoomGuy', frags: 25, deaths: 0},
-  {name: 'Slayer', frags: 25, deaths: 1},
-  {name: 'Marine', frags: 12, deaths: 15},
-  {name: 'Noob', frags: 0, deaths: 25},
-]
+    {name: 'DoomGuy', frags: 25, deaths: 0},
+    {name: 'Slayer', frags: 25, deaths: 1},
+    {name: 'Marine', frags: 12, deaths: 15},
+    {name: 'Noob', frags: 0, deaths: 25},
+  ];
+//The reduce method is used to calculate a single value from frags of all players or the total number of frags
+  const totalFrags = players.reduce((total, players) => {
+    return total + players.frags; //This goes through each player and adds the frags to the total
+  }, 0); //This is the starting value of the total
 
-const playersWithMoreFragsThanDeaths = players.filter((player) => {
-  console.log(`We are now looking at this player: ${player.name}`) //This is a fancy way of telling you that its iterating through each player
-  return player.frags > player.deaths
-})
+console.log(totalFrags);
 
-console.log(playersWithMoreFragsThanDeaths)
+//This is a shortened version of the code above using an implicit return
+const totalFragsTwo = players.reduce((total, player) => total + player.frags, 0)
 
-//OR
-//This is a shortened version of the code above
-const playersWithMoreFragsThanDeathsTwo = players.filter((player) => player.frags > player.deaths)// This skips the "we are now looking at this player" part and just tells you the players
-console.log(playersWithMoreFragsThanDeathsTwo)
+console.log(totalFragsTwo)
+
+
