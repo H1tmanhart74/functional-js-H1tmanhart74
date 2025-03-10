24881d5760709c13f7698b46516d8eb32fef0e9f
John Hart <john.hart003@student.swic.edu>
2025-03-09 23:46:29 -0500

    Add some method example code

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -1,4 +1,4 @@
-//This is an example for the find method
+//This is an example for the some method
 //Our classic doom players example
 const players = [
     {name: 'DoomGuy', frags: 25, deaths: 0},
@@ -6,9 +6,8 @@ const players = [
     {name: 'Marine', frags: 12, deaths: 15},
     {name: 'Noob', frags: 0, deaths: 25},
   ];
-//The find method will return the first player that has more frags than deaths
-  const firstPlayerWithMoreFragsThanDeaths = players.find(player => player.frags > player.deaths); //This will go through the array and returns the first thing that matches the condition
+//The some method will return a boolean value if it passes the condition
+//In this case we are checking if there are any frags
+const hasFrags = players.some((player) => player.frags);
 
-  console.log(firstPlayerWithMoreFragsThanDeaths); //Answer is DoomGuy with more frags than deaths
-
-
+console.log(hasFrags); //Since there are frags it is true
